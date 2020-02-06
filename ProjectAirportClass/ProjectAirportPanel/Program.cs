1eba4531ce460ffc0cd3e4600dcf8f1c359d9e74
Андреев Андрей <glaz9097@gmail.com>
2020-02-06 16:07:50 +0200

    Готово!

--- ProjectAirportClass/ProjectAirportPanel/Program.cs
+++ ProjectAirportClass/ProjectAirportPanel/Program.cs
@@ -165,6 +165,14 @@ namespace ProjectAirportClass
             {
                 FlightsActions.EditPassengerInfo(Flights);
             }
+            else if (option == FlightsActions.NameOfActions.DeletePassenger)
+            {
+                FlightsActions.DeletePassenger(Flights);
+            }
+            else if (option == FlightsActions.NameOfActions.DeleteFlightPrice)
+            {
+                FlightsActions.DeleteFlightPrice(Flights);
+            }
             else if (option == FlightsActions.NameOfActions.WatchThePassengerList)
             {
                 FlightsActions.WatchThePassengerList(Flights);
@@ -201,6 +209,8 @@ namespace ProjectAirportClass
             SearchPassenger,
             EditPassengerInfo,
             EditFlightPriceListInfo,
+            DeletePassenger,
+            DeleteFlightPrice,
             WatchTheFlightsPriceList,
             WatchThePassengerList
         }
@@ -535,6 +545,21 @@ namespace ProjectAirportClass
 
         public static void EditPassengerInfo(SortedList<string, Airoport> Flights)
         {
+            DeletePassenger(Flights);
+
+            try
+            {
+                AddPassengerToTheFlight(Flights);
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка при попытке обработки информации! Попробуйте еще раз!");
+                Console.ReadKey();
+            }
+        }
+
+        public static void DeletePassenger(SortedList<string, Airoport> Flights)
+        {
             Console.WriteLine("Введите номер рейса для редактирования");
             var FlightNumber = Console.ReadLine();
 
@@ -546,7 +571,29 @@ namespace ProjectAirportClass
                 var ArrayPassangers = Flights.Values.Where(x => x.FlightNumber == FlightNumber).Select(x => x.ListOfPassengers).First();
 
                 ArrayPassangers.Remove(ArrayPassangers.Where(x => x.PassportNumber == PassportNumber).Select(x => x).First());
-                AddPassengerToTheFlight(Flights);
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка при попытке обработки информации! Попробуйте еще раз!");
+                Console.ReadKey();
+            }
+        }
+
+        public static void DeleteFlightPrice(SortedList<string, Airoport> Flights)
+        {
+            Console.WriteLine("Введите номер рейса для редактирования");
+            var FlightNumber = Console.ReadLine();
+
+            Console.WriteLine("Введите номер класса цен для редактирования (0 - Econom, 1 - Bussines, 2 - BussinesPlus)");
+            int.TryParse(Console.ReadLine(), out int NumberOfPriceClass);
+
+            Passenger.ClassOfFlight EnteredEnum = (Passenger.ClassOfFlight)Enum.Parse(typeof(Passenger.ClassOfFlight), NumberOfPriceClass.ToString());
+
+            try
+            {
+                var ArrayFlights = Flights.Values.Where(x => x.FlightNumber == FlightNumber).Select(x => x.PriceList).First();
+
+                ArrayFlights.Remove(EnteredEnum);
             }
             catch
             {
@@ -605,8 +652,7 @@ namespace ProjectAirportClass
                 {"UA228-1488", new Airoport("Zhytomyr", "Kyiv", "Airoport-1", "Boryspil", "UA228-1488", "D", "1D", new DateTime(2019, 12, 15), new DateTime(2019, 12, 15), new DateTime(2019, 12, 14), Airoport.FlightStatus.InFlight, FlightsActions.CreateNewListOfPasssangers(rnd), BaseFunctions.GetBasePrices(rnd) ) },
                 {"UA229-1488", new Airoport("Lviv", "Kyiv", "Airoport-3", "Boryspil", "UA229-1488", "A", "3D", new DateTime(2019, 12, 16), new DateTime(2019, 12, 16), new DateTime(2019, 12, 13), Airoport.FlightStatus.Delayed, FlightsActions.CreateNewListOfPasssangers(rnd), BaseFunctions.GetBasePrices(rnd) ) },
                 {"UA222-1488", new Airoport("Kharjiv", "Kyiv", "Airoport-4", "Boryspil", "UA222-1488", "C", "4D", new DateTime(2019, 12, 16), new DateTime(2019, 12, 16), new DateTime(2019, 12, 13), Airoport.FlightStatus.Canceled, FlightsActions.CreateNewListOfPasssangers(rnd), BaseFunctions.GetBasePrices(rnd)  ) },
-                {"UA221-1488", new Airoport("Odessa", "Kyiv", "Airoport-5", "Boryspil", "UA221-1488", "B", "5D", new DateTime(2019, 12, 16), new DateTime(2019, 12, 16), new DateTime(2019, 12, 13), Airoport.FlightStatus.Delayed, FlightsActions.CreateNewListOfPasssangers(rnd), BaseFunctions.GetBasePrices(rnd) )},
-                {"UA225-1488", new Airoport("Sumy", "Kyiv", "Airoport-6", "Boryspil", "UA225-1488", "G", "6D", new DateTime(2019, 12, 16), new DateTime(2019, 12, 16), new DateTime(2019, 12, 13), Airoport.FlightStatus.Delayed, FlightsActions.CreateNewListOfPasssangers(rnd), BaseFunctions.GetBasePrices(rnd) )}
+                {"UA221-1488", new Airoport("Odessa", "Kyiv", "Airoport-5", "Boryspil", "UA221-1488", "B", "5D", new DateTime(2019, 12, 16), new DateTime(2019, 12, 16), new DateTime(2019, 12, 13), Airoport.FlightStatus.Delayed, FlightsActions.CreateNewListOfPasssangers(rnd), BaseFunctions.GetBasePrices(rnd) )}
             };
 
             while (true)
@@ -619,8 +665,9 @@ namespace ProjectAirportClass
                     "Введите 4 - Поиск по элементу строки перелета;", "Введите 5 - Поиск ближайшего перелета в определенный Город;",
                     "Введите 6 - Вывод срочной информации на панель!;", "Введите 7 - Добавить пассажира на рейс;",
                     "Введите 8 - Поиск пассажира по ключевым значениям;", "Введите 9 - Изменить информацию о пасажире;",
-                    "Введите 10 - Изменить информацию цен на рейс;", "Введите 11 - Вывод информации о ценах классов перелетов;",
-                    "Введите 12 - Вывод списка пассажиров;", "Введите 0 - Для выхода из программы."));
+                    "Введите 10 - Изменить информацию цен на рейс;", "Введите 11 - Удалить пассажира с рейса;",
+                    "Введите 12 - Удалить цену на рейс;", "Введите 13 - Вывод информации о ценах классов перелетов;",
+                    "Введите 14 - Вывод списка пассажиров;", "Введите 0 - Для выхода из программы."));
 
                 int.TryParse(Console.ReadLine(), out int option);
 
