77bdb7c4cc1f77d62dd2d34f93cda7f07e0a4a79
Андреев Андрей <glaz9097@gmail.com>
2020-02-19 19:54:09 +0200

    ГОТОВО!

---
+++ Part 2 LabWork 1.1/Part 2 LabWork 1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using PersonLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2_LabWork_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person student = new Person();
+
+            student.FirstName = "Xep";
+            student.LastName = "SGory";
+            student.BirthDay = DateTime.Now;
+            student.ID = 1;
+
+            Console.WriteLine("Student FirstName " + student.FirstName +
+                "Student LastName " + student.LastName + "Student BirthDay " + student.BirthDay);
+
+            Console.ReadKey();
+        }
+    }
+}
