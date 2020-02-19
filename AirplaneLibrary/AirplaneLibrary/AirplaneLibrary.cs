7df3ded7e75a7266d592bf1b695b9adf55a6425a
Андреев Андрей <glaz9097@gmail.com>
2020-02-19 21:10:36 +0200

    Готово!

---
+++ AirplaneLibrary/AirplaneLibrary/AirplaneLibrary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirplaneLibrary
+{
+    public enum AirplaneTypes
+    {
+        SportPlane,
+        CargoPlane,
+        TurboProp,
+        Jet
+    }
+
+    public class AirplaneTypeAttribute
+    {
+        public AirplaneTypes Type;
+
+        public AirplaneTypeAttribute()
+        {
+            Type = AirplaneTypes.TurboProp;
+        }
+
+        public AirplaneTypeAttribute(AirplaneTypes Type)
+        {
+            this.Type = Type;
+        }
+    }
+
+    public class AirplaneLibrary
+    {
+
+    }
+}
---
+++ Part 2 LabWork 1.2/Part 2 LabWork 1.2/Program.cs
@@ -0,0 +1,96 @@
+﻿using AirplaneLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2_LabWork_1._2
+{
+    public class UniversalAirplane
+    {
+        public AirplaneTypes Jet = AirplaneTypes.Jet;
+        public AirplaneTypes SportPlane = AirplaneTypes.SportPlane;
+        public AirplaneTypes TurboProp = AirplaneTypes.TurboProp;
+
+        public UniversalAirplane()
+        {
+            Console.Write("This is  universal airplane class contains types: " + Jet + " " + SportPlane + " " + TurboProp);
+        }
+    }
+
+    public class CargoAirplane
+    {
+        public AirplaneTypes CargoPlane = AirplaneTypes.CargoPlane;
+
+        public CargoAirplane()
+        {
+            Console.Write("This is  cargo airplane class contains type: " + CargoPlane);
+        }
+    }
+
+    public class CheckSaveTrace
+    {
+        public static void CheckClassAtributes(object Class)
+        {
+            //вывод атрибутов и их значений
+
+            Type myType = Type.GetType(Class.ToString(), false, true);
+
+            foreach (var mi in myType.GetMembers())
+            {
+                Console.WriteLine($"{mi.DeclaringType} {mi.MemberType} {mi.Name}");
+            }
+        }
+
+        public static string GetDescriptionOfAttributes(object Class)
+        {
+            var ReturnString = "";
+
+            Type myType = Type.GetType(Class.ToString(), false, true);
+
+            foreach (var mi in myType.GetMembers())
+            {
+                ReturnString = ReturnString + $"{mi.DeclaringType} {mi.MemberType} {mi.Name}";
+            }
+
+            return ReturnString;
+        }
+
+        public static void SaveTrace()
+        {
+            //сохранение данных в Trace file
+
+            var consoleTracer = new ConsoleTraceListener();
+
+            consoleTracer.Name = "mainConsoleTracer";
+
+            consoleTracer.WriteLine(GetDescriptionOfAttributes(new UniversalAirplane()) + " " + consoleTracer.Name );
+        }
+
+        public static void EventLogging()
+        {
+            //сохранение данных в event log
+
+            var appLog = new EventLog("Application");
+            appLog.Source = "MySource";
+            appLog.WriteEntry(GetDescriptionOfAttributes(new UniversalAirplane()));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckSaveTrace.GetDescriptionOfAttributes(new UniversalAirplane());
+
+            CheckSaveTrace.SaveTrace();
+
+            CheckSaveTrace.EventLogging();
+
+            Console.ReadKey();
+        }
+    }
+}
