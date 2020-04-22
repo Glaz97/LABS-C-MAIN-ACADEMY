872663f2994979d395ae98e08a031cb8005b71f6
Андреев Андрей <glaz9097@gmail.com>
2020-04-22 23:14:50 +0300

    ГОТОВО!

---
+++ Part 2 LabWork 6.1/Part 2 LabWork 6.1/IService1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Part_2_LabWork_6._1
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        double Addition(int a, int b);
+
+        [OperationContract]
+        double Subtraction(int a, int b);
+
+        [OperationContract]
+        double Multiplication(int a, int b);
+
+        [OperationContract]
+        double Division(int a, int b);
+    }
+
+
+    // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
